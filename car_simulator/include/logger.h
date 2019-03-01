fcdcd68d72e82268c2cd8fb01f7ea183b40638e9
Davide Spataro <davide90.spataro@gmail.com>
2019-03-02 00:25:43 +0100

    add command line parse of parameters, command line parameters parser, logger to file

---
+++ car_simulator/include/logger.h
@@ -0,0 +1,68 @@
+#ifndef __LOGGER__
+#define __LOGGER__
+
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+namespace Log {
+
+class Logger{
+	public:
+		Logger(){};
+	 virtual void log(const std::string& s) = 0;
+};
+
+class FileLogger : public Logger {
+ public:
+  FileLogger(const std::string& filepath) {
+    try {
+      logfile.open(filepath+get_timestamp("%F_%T"));
+    } catch (const std::ifstream::failure& e) {
+      std::cerr << "Exception opening/reading file";
+      std::cerr << e.what() << std::endl;
+    }
+  }
+
+  virtual void log(const std::string& s) override{
+    try {
+      const std::string line = get_timestamp() + "\t" + s;
+      logfile << line << std::endl;
+    } catch (const std::ifstream::failure& e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+
+  FileLogger(){
+  	logfile.close();
+  }
+
+ private:
+  std::ofstream logfile;
+
+  std::string get_timestamp(const std::string& format = "%F %T") {
+    const std::chrono::system_clock::time_point now =
+        std::chrono::system_clock::now();
+    const std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream transTime;
+    transTime << std::put_time(std::localtime(&now_c), format.c_str());
+    return transTime.str();
+  }
+};
+
+class FileCerrLogger : public FileLogger{
+public:
+	FileCerrLogger(const std::string& filepath) : FileLogger(filepath){};
+
+	void log(const std::string& s) override {
+		std::cerr<<s<<std::endl;
+		FileLogger::log(s);
+	}
+};
+
+}  // namespace Log
+
+#endif  //__LOGGER__
--- car_simulator/source/server.cpp
+++ car_simulator/source/server.cpp
@@ -1,73 +1,131 @@
-#include <iostream>
-#include <memory>
+#include <cxxopts.h>
+#include <logger.h>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
-
+#include <iostream>
+#include <memory>
+#include <tuple>
 
 using boost::asio::ip::tcp;
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
 
 constexpr size_t MAX_LEN = 1024;
-class Server : public std::enable_shared_from_this<Server>{
-public:
-  Server(boost::asio::io_service& ios,
-         short port) : ios(ios), acceptor(ios, tcp::endpoint(tcp::v4(), port)), socket(ios)
-  {
+class Server : public std::enable_shared_from_this<Server> {
+ public:
+  Server(boost::asio::io_service& ios, short port, Log::Logger* const logger)
+      : ios(ios),
+        acceptor(ios, tcp::endpoint(tcp::v4(), port)),
+        socket(ios),
+        logger(logger) {
     acceptor.async_accept(socket,
                           boost::bind(&Server::handle_accept, this,
                                       boost::asio::placeholders::error));
   }
 
-  void handle_accept(const boost::system::error_code& err)
-  {
+  void handle_accept(const boost::system::error_code& err) {
     if (!err) {
-    std::string s = socket.remote_endpoint().address().to_string();
-    std::cout<<"Connected to "<<s<<std::endl;
-	socket.async_read_some(
-	          boost::asio::buffer(data, MAX_LEN),
-	          boost::bind(&Server::handle_read, this,
-	                      boost::asio::placeholders::error,
-							boost::asio::placeholders::bytes_transferred)
-	          );
-    }
-    else {
+      std::string s = socket.remote_endpoint().address().to_string();
+      logger->log("Connected to " + s);
+      socket.async_read_some(
+          boost::asio::buffer(data, MAX_LEN),
+          boost::bind(&Server::handle_read, this,
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
+    } else {
       std::cerr << "err: " + err.message() << std::endl;
     }
   }
 
-	void handle_read(
-	  const boost::system::error_code& error, // Result of operation.
-	  std::size_t bytes_transferred           // Number of bytes read.
-	){
-		for(int i = 0 ; i < bytes_transferred ; i++)
-			std::cout<<data[i];
-		std::cout<<std::endl;
-
-	}
-
-
-  tcp::socket& get_socket()
-  {
-    return socket;
+  void handle_read(
+      const boost::system::error_code& error,  // Result of operation.
+      std::size_t bytes_transferred            // Number of bytes read.
+  ) {
+    for (unsigned i = 0; i < bytes_transferred; i++) std::cout << data[i];
+    std::cout << std::endl;
   }
 
-private:
-  tcp::socket socket;
+  tcp::socket& get_socket() { return socket; }
+
+ private:
   boost::asio::io_service& ios;
   tcp::acceptor acceptor;
-  
+  tcp::socket socket;
   char data[MAX_LEN];
+
+  const std::shared_ptr<Log::Logger> logger;
 };
 
-int main(int argc, char *argv[])
-{
+struct net_config {
+  std::string address;
+  std::string port;
+
+  std::string to_string() {
+    std::ostringstream ss;
+    ss << "Net config" << std::endl;
+    if(address != "")
+      ss << "address: "<< address << std::endl;
+    else
+      ss<<"Warning: address not specified"<< std::endl;
+    if(port != "")
+      ss << "port: "<< port << std::endl;
+    else
+      ss<<"Warning: port not specified"<< std::endl;
+    
+    return ss.str();
+  }
+};
+
+using LogFile = std::string;
+std::tuple<net_config, LogFile> create_config_from_comm_ln_options(
+    int argc, char* argv[]) {
+  try {
+    cxxopts::Options options(argv[0], " - example command line options");
+    options.add_options()("s,server-address", "Address" , cxxopts::value<std::string>())
+    ("p,port", "Port", cxxopts::value<std::string>())
+    ("f,file-log", "Log File" , cxxopts::value<std::string>());
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+      std::cout << options.help({""}) << std::endl;
+      exit(0);
+    }
+
+    net_config config;
+    std::string logfile;
+    if (result.count("s")) {
+      config.address = result["s"].as<std::string>();
+    }
+    if (result.count("p")) {
+      config.port = result["p"].as<std::string>();
+
+    }
+    if (result.count("f")) {
+       logfile = result["f"].as<std::string>();
+    }
+
+    return std::make_tuple(config, logfile);
+
+  } catch (const cxxopts::OptionException& e) {
+    std::cout << "error parsing options: " << e.what() << std::endl;
+    exit(1);
+  }
+  return {};
+}
+
+int main(int argc, char* argv[]) {
+  std::tuple<net_config, LogFile> parse_result =
+      create_config_from_comm_ln_options(argc, argv);
+
+  std::shared_ptr<Log::Logger> logger(new Log::FileCerrLogger("log.txt"));
+  logger->log(std::get<net_config>(parse_result).to_string());
   try {
     boost::asio::io_service ios;
-    Server s(ios, 35000);
+    Server s(ios, 35000, logger.get());
     ios.run();
-  } catch(std::exception& e) {
+  } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
   return 0;
